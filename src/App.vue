c417b888e9665e414ec29ef18256900f2daf07b3
anto-iorfida <antonioiorfida@hotmail.com>
2024-06-10 16:31:10 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script >
+import ProjectList from './components/ProjectList.vue'
+
+export default{
+  components:{
+    ProjectList
+  }
+}
+</script>
+
+<template>
+<ProjectList></ProjectList>
+</template>
+
+<style lang="scss" >
+@use './style/generic';
+
+</style>
---
+++ src/components/ProjectList.vue
@@ -0,0 +1,35 @@
+<script>
+export default {
+    name: "ProjectList",
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+
+<template>
+    <div class="container">
+        <h2>All Projects</h2>
+
+        <div class="row row-cols-4">
+            <div class="col">
+                <div class="card my-3" style="width: 18rem;">
+                    <div class="card-body">
+                        <h4 class="card-title">Card title</h4>
+                        <h6 class="card-subtitle my-3 text-body-secondary">Card subtitle</h6>
+                        <p class="card-text">
+                            Some quick example text to build on the card title and make up the bulk of
+                            the card's content.
+                        </p>
+                        <!-- <a href="#" class="card-link">Card link</a>
+                        <a href="#" class="card-link">Another link</a> -->
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
