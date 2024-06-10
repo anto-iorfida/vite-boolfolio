c075a9faa0e4ccf68f83a1c22db7d4d19063d77f
anto-iorfida <antonioiorfida@hotmail.com>
2024-06-10 16:44:53 +0200

    creation of cards and printouts

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -1,10 +1,23 @@
 <script>
+import axios from 'axios';
+
 export default {
     name: "ProjectList",
     data() {
         return {
-
+            projects: []
+        };
+    },
+    methods:{
+        getProjects() {
+            axios.get('http://127.0.0.1:8000/api/projects')
+            .then((response) => {
+                this.projects = response.data.result;
+            });
         }
+    },
+    mounted () {
+        this.getProjects();
     }
 }
 </script>
@@ -14,15 +27,12 @@ export default {
         <h2>All Projects</h2>
 
         <div class="row row-cols-4">
-            <div class="col">
-                <div class="card my-3" style="width: 18rem;">
+            <div v-for="project in projects" :key="project.id" class="col">
+                <div class="card my-3"  style="width: 18rem;">
                     <div class="card-body">
-                        <h4 class="card-title">Card title</h4>
-                        <h6 class="card-subtitle my-3 text-body-secondary">Card subtitle</h6>
-                        <p class="card-text">
-                            Some quick example text to build on the card title and make up the bulk of
-                            the card's content.
-                        </p>
+                        <h4 class="card-title">{{project.name}}</h4>
+                        <h6 class="card-subtitle my-3 text-body-secondary">{{project.client_name}}</h6>
+                        <p class="card-text">{{ project.summary }}</p>
                         <!-- <a href="#" class="card-link">Card link</a>
                         <a href="#" class="card-link">Another link</a> -->
                     </div>
