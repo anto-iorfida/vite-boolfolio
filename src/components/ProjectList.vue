66de7152148cdfe42766b22dc8693ee95428d905
anto-iorfida <antonioiorfida@hotmail.com>
2024-06-11 09:18:27 +0200

    feat add  page management

--- src/components/ProjectList.vue
+++ src/components/ProjectList.vue
@@ -4,24 +4,36 @@ import SingleProject from './SingleProject.vue'
 
 export default {
     name: "ProjectList",
-    components:{
+    components: {
         SingleProject
     },
     data() {
         return {
-            projects: []
+            currentPage: 1,
+            prevPageUrl: null,
+            nextPageUrl: null,
+            projects: [],
         };
     },
-    methods:{
-        getProjects() {
-            axios.get('http://127.0.0.1:8000/api/projects')
-            .then((response) => {
-                this.projects = response.data.result;
-            });
+    methods: {
+        getProjects(pageNumber) {
+            axios.get('http://127.0.0.1:8000/api/projects',{
+                params:{
+                    page : pageNumber
+                }
+            })
+
+                .then((response) => {
+                    this.projects = response.data.result.data;
+                    this.currentPage = response.data.result.current_page;
+                    this.prevPageUrl = response.data.result.prev_page_url;
+                    this.nextPageUrl = response.data.result.next_page_url;
+                });
         }
+
     },
-    mounted () {
-        this.getProjects();
+    mounted() {
+        this.getProjects(this.currentPage);
     }
 }
 </script>
@@ -30,9 +42,22 @@ export default {
     <div class="container">
         <h2>All Projects</h2>
 
-        <div class="row row-cols-4">
-           <SingleProject v-for="project in projects" :key="project.id" :projectInfo="project" ></SingleProject>
+        <div class="row row-cols-4 my-5">
+            <SingleProject v-for="project in projects" :key="project.id" :projectInfo="project"></SingleProject>
         </div>
+        <nav aria-label="Page navigation example">
+            <ul class="pagination justify-content-center">
+                <li v-if="prevPageUrl" class="page-item ">
+                    <a class="page-link rounded-pill" @click="getProjects(currentPage - 1)">Previous</a>
+                </li>
+                <li class="page-item"><a class="page-link rounded-circle" >{{ currentPage - 1 }}</a></li>
+                <li class="page-item "><a class="page-link rounded-circle large" >{{ currentPage }}</a></li>
+                <li class="page-item"><a class="page-link rounded-circle" >{{ currentPage + 1 }}</a></li>
+                <li v-if="nextPageUrl" class="page-item ">
+                    <a class="page-link rounded-pill" @click="getProjects(currentPage + 1)">Next</a>
+                </li>
+            </ul>
+        </nav>
     </div>
 </template>
 
